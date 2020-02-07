a7f36ccbfd6640245d10ce04a236b2484614d990
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-07 02:23:42 +0000

    if successful patch /animals/:id redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,32 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
-
+  
+  get "animals/new" do 
+    erb :new 
+  end 
+  
+  post "/animals" do 
+    @animal = Animal.create(params)
+    redirect "/animals/#{@animal.id}"
+  end 
+  
+  get "/animals" do 
+    @animal = Animal.all
+    erb :index 
+  end 
+  
+  get "/animals/:id" do 
+    @animal = Animal.find_by_id(params[:id])
+    erb :show 
+  end 
+  
+  get "/animals/:id/edit" do 
+    @animal = Animal.find_by_id(params[:id])
+    erb :edit 
+  end 
+  
+  patch "/animals/:id" do 
+    # @animal = Animal.find(params[:id], :name = params[:name], params[:species], params[])
+  end 
 end
